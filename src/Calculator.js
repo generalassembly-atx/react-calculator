7ed3d9d77ddc2998c72d0ecf804d001df7a19671
jesse houghton <OiseauNoir>
2016-09-08 16:27:18 -0500

    initial commit

---
+++ src/Calculator.js
@@ -0,0 +1,11 @@
+<div className="container">
+  <h1>Add with React!</h1>
+
+  <div className="add">
+    <input type="text" />
+    <span>+</span>
+    <input type="text" />
+    <span>=</span>
+    <h3>Addition results go here!</h3>
+  </div>
+</div>
