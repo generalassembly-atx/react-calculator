105f5e42916657a4eade4b9b423e622a5c6cf0bf
jesse houghton <OiseauNoir>
2016-09-08 20:25:05 -0500

    getting hotter

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react';
 
 
-class Calculator extends Component {
+  class Calculator extends Component {
+    constructor(props) {
+    super(props)
+    this.handleSubmit=this.handleSubmit.bind(this)
+    this.reset=this.reset.bind(this)
+    this.state = {
+      input: null
+    }
+  }
 
+  handleSubmit(e) {
+    e.preventDefault()
+    var firstInterger = parseFloat(this.refs.inputFirst.value)
+    var secondInterger = parseFloat(this.refs.inputSecond.value)
+    var sum = (firstInterger + secondInterger);
+    this.setState({
+      sum: sum
+    })
+  }
+
+  reset(e) {
+    e.preventDefault()
+    this.setState({
+    sum: null
+    })
+      this.refs.inputFirst.value = ''
+      this.refs.inputSecond.value = ''
+  }
 
   render() {
     return (
       <div className="container">
         <h1>Add with React!</h1>
 
-        <div className="inputs">
+        <form onSubmit={this.handleSubmit}>
           <input type="text" ref="inputFirst" placeholder="Input 1"/>
           <span>+</span>
           <input type="text" ref="inputSecond" placeholder="Input 2"/>
           <span>=</span>
           <h3>Addition results go here!</h3>
-        </div>
+          <input type="submit" value="Calculate!" />
+          <button onClick={this.reset}>Reset</button>
+        </form>
       </div>
     )
   }
