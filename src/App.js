96557d93c096a0f886a0d193072f11cb4127d750
Chris Robbins <Jaguar85>
2016-09-09 09:10:23 -0500

    calculator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Calculator from './Calculator'
 class App extends Component {
 
   render() {
