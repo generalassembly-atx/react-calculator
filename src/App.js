314431982f3cd0f8de089a61cb64eaf31b528b06
Jeanette Adelson <jeni.jeanette@gmail.com>
2016-09-09 10:40:02 -0500

    completed react-calculator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.getTotal = this.getTotal.bind(this)
+
+    this.state = {
+      total: null
+    }
+
+  }
+
+  getTotal() {
+    var first = parseFloat(this.refs.first.value)
+    var second = parseFloat(this.refs.second.value)
+    var total = (first + second)
+    console.log(total);
+    this.setState({
+      total: total
+    })
+  }
 
   render() {
     return (
-      <Calculator />
+      <div className="container">
+        <h1>Add with React!</h1>
+
+        <form onKeyUp={this.getTotal}>
+          <input type='text' ref='first'/>
+          <span>+</span>
+          <input type='text' ref='second'/>
+          <span>=</span>
+          <h3>{this.state.total}</h3>
+          {/* <button></button> */}
+        </form>
+      </div>
     );
   }
 }
