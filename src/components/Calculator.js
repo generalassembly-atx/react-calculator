415bd9dbb5e83fcd8392af705b48fc90b382a9c8
Sean Haughey <sdhaughey@gmail.com>
2016-09-08 19:35:54 -0500

    added bonus solution

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -16,7 +16,31 @@ class Calculator extends Component {
     this.setState({
       sum: sum
     });
-  }
+  };
+
+  subtract() {
+    var sum = parseInt(this.refs.val1.value) - parseInt(this.refs.val2.value);
+
+    this.setState({
+      sum: sum
+    });
+  };
+
+  multiply() {
+    var sum = parseInt(this.refs.val1.value) * parseInt(this.refs.val2.value);
+
+    this.setState({
+      sum: sum
+    });
+  };
+
+  divide() {
+    var sum = parseInt(this.refs.val1.value) / parseInt(this.refs.val2.value);
+
+    this.setState({
+      sum: sum
+    });
+  };
 
   render() {
     return (
@@ -24,10 +48,15 @@ class Calculator extends Component {
         <h1>Add with React!</h1>
 
         <div className="add">
-          <input type="text" ref="val1" onKeyUp={this.add.bind(this)} />
-          <span>+</span>
-          <input type="text" ref="val2" onKeyUp={this.add.bind(this)} />
+          <input type="text" ref="val1" />
+          <input type="text" ref="val2" />
           <span>=</span>
+          <div className="buttons">
+            <button id="add" onClick={this.add.bind(this)}>+</button>
+            <button id="subtract" onClick={this.subtract.bind(this)}>-</button>
+            <button id="multiply" onClick={this.multiply.bind(this)}>x</button>
+            <button id="divide" onClick={this.divide.bind(this)}>/</button>
+          </div>
           <h3>{this.state.sum}</h3>
         </div>
       </div>
