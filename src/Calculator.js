a909f0910b7dc2567ae925b017a5179942df0acf
Thomas Foster <thomasfoster90>
2016-11-01 08:45:35 -0500

    homework

---
+++ src/Calculator.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Calculator extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      sum:0
+    }
+  }
+  add(e){
+    e.preventDefault();
+    let add1 =this.refs.add1.value
+    let add2=this.refs.add2.value
+    this.setState({
+      sum:+add1 + +add2
+    });
+  }
+  render(){
+    return(
+      <div className="container">
+  <h1>Add with React!</h1>
+
+  <div className="add">
+    <input type="text" ref="add1" onChange={(e) => this.add(e)} />
+    <span>+</span>
+    <input type="text" ref="add2" onChange={(e) => this.add(e)} />
+    <span>=</span>
+    <h3>Addition results go here!</h3>
+    <span> {this.state.sum}</span>
+  </div>
+</div>
+    )
+  }
+}
+
+export default Calculator;
