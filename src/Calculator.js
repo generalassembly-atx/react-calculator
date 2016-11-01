3d5ef571355d4f26d7297ffd643e5a5859a54883
Aaron Baker <aaronbaker9210@gmail.com>
2016-10-31 19:08:25 -0500

    addition working on change

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,17 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Calculator extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      sum: 0
+    }
+  }
+  add(e){
+    e.preventDefault();
+    var add1 = this.refs.add1.value
+    var add2 = this.refs.add2.value
+    this.setState({
+      sum:+add1 + +add2
+    });
+  }
+
   render() {
     return(
       <div className="container">
         <h1>Add with React!</h1>
 
         <div className="add">
-          <input type="text" />
-          <span>+</span>
-          <input type="text" />
-          <span>=</span>
-          <h3>Addition results go here!</h3>
+          <input type="text" ref="add1" onChange={(e) => this.add(e)} />
+          <span> + </span>
+          <input type="text" ref="add2" onChange={(e) => this.add(e)} />
+          <span> = </span>
+          <span> {this.state.sum}</span>
         </div>
       </div>
     )
